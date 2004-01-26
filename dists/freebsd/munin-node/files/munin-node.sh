c86dcb62c96eba6668d7f76d5b434cd49c53a692
Lupe Christoph <lupe@lupe-christoph.de>
2004-01-26 13:20:01 +0000

    Adding FreeBSD ports to the supported distributions.

---
+++ dists/freebsd/munin-node/files/munin-node.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+PROGRAM=%PREFIX%/sbin/munin-node
+CONFIG=%PREFIX%/etc/munin/munin-node.conf
+
+case "$1" in
+	start)
+		if [ -x $PROGRAM -a -f $CONFIG -a -d $PLUGINS ]; then
+			$PROGRAM --config $CONFIG && echo -n ' munin-node'
+		fi
+	;;
+
+	stop)
+		if [ -f $CONFIG ]; then
+			PIDFILE=`awk '$1 == "pid_file" { print $2 }' /usr/local/etc/munin/client.conf`
+			if [ -f $PIDFILE ]; then
+				/bin/kill `cat $PIDFILE` && echo -n ' munin-node'
+			fi
+		fi
+	;;
+	
+	*)
+		echo "Usage: `basename $0` { start | stop }"
+		exit 64
+	;;
+esac
