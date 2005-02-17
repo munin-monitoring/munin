fc16e1c81a68a23485a7f1265cf7afb9ccb2ca80
Automatic Commit <no-one@munin-monitoring.org>
2005-02-17 15:47:52 +0000

    This commit was manufactured by cvs2svn to create tag '1.2.0'.

--- dists/freebsd/munin-node/files/munin-node-revive.sh
+++ dists/freebsd/munin-node/files/munin-node-revive.sh
@@ -1,28 +1,27 @@
 #!/bin/sh
-#
-
-# PROVIDE: munin-node
-# REQUIRE: DAEMON
-# BEFORE:  cron
-# KEYWORD: FreeBSD
 
 #
 # Add the following lines to /etc/rc.conf to enable munin-node:
 # munin_node_enable (bool):      Set to "NO" by default.
 #                                Set it to "YES" to enable munin-node
-# munin_node_config (path):      Set to "%PREFIX%/etc/munin/munin-node.conf" by default.
+# munin_node_config (path):      Set to "%%PREFIX%%/etc/munin/munin-node.conf" by default.
 #
 
-. /etc/rc.subr
+. %%RC_SUBR%%
 
 name="munin_node"
 rcvar=`set_rcvar`
 
 [ -z "$munin_node_enable" ] && munin_node_enable="NO"
-[ -z "$munin_node_config" ] && munin_node_config="/usr/local/etc/munin/munin-node.conf"
+[ -z "$munin_node_config" ] && munin_node_config="%%PREFIX%%/etc/munin/munin-node.conf"
 
-command="/usr/local/sbin/munin-node"
+command="%%PREFIX%%/sbin/munin-node"
 pidfile=`awk '$1 == "pid_file" { print $2 }' $munin_node_config`
 
 load_rc_config $name
-run_rc_command "$1"
+
+#set -x
+pid=`check_pidfile $pidfile %%PREFIX%%/sbin/munin-node`
+if [ -z "$pid" ]; then
+  run_rc_command start
+fi
