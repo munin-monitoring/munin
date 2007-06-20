5b22070bdd554521aebe9ae5040585582101b91f
Ingvar Hagelund <ingvar@redpill-linpro.com>
2007-06-20 13:49:32 +0000

    According to munin co-author Jimmy, the daily munin-limits cron job is nagios specific

--- dists/redhat/munin.cron.d
+++ dists/redhat/munin.cron.d
@@ -5,4 +5,4 @@
 MAILTO=root
 
 */5 * * * *     munin test -x /usr/bin/munin-cron && /usr/bin/munin-cron
-14 10 * * *     munin test -x /usr/share/munin/munin-limits && /usr/share/munin/munin-limits --force
+14 10 * * *     munin test -x /usr/share/munin/munin-limits && /usr/share/munin/munin-limits --force --contact nagios --contact old-nagios
