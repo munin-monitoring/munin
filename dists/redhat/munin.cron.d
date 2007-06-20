0384e72540805442ee23a144f5c3e564ebf2d6d5
Ingvar Hagelund <ingvar@redpill-linpro.com>
2007-06-20 11:51:29 +0000

    We must have a cron job for munin-limits

--- dists/redhat/munin.cron.d
+++ dists/redhat/munin.cron.d
@@ -5,3 +5,4 @@
 MAILTO=root
 
 */5 * * * *     munin test -x /usr/bin/munin-cron && /usr/bin/munin-cron
+14 10 * * *     munin test -x /usr/share/munin/munin-limits && /usr/share/munin/munin-limits --force
