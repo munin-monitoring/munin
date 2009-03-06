99f671bb7b8fe3cb4aca48ec986f9cc6d9a877cc
Kjell-Magne Øierud <kjellm@redpill-linpro.com>
2009-03-06 13:05:54 +0000

    - Moved get_fq_hostname to Munin::OS - Put argument parsing in a sub

--- node/lib/Munin/OS.pm
+++ node/lib/Munin/OS.pm
@@ -17,6 +17,20 @@ sub get_gid {
     return $group =~ /\d/ ? $group : getgrnam($group);
 }
 
+
+sub get_fq_hostname {
+    my $hostname = eval {
+        require Sys::Hostname;
+        return (gethostbyname(Sys::Hostname::hostname()))[0];
+    };
+    return $hostname if $hostname;
+
+    $hostname = `hostname`;
+    chomp($hostname);
+    $hostname =~ s/\s//g;
+    return $hostname;
+}
+
 1;
 
 __END__
@@ -42,5 +56,9 @@ Returns the user ID. $user might either be a user name or a user ID.
 =item $gid = $class->get_gid($group)
 
 Returns the group ID. $group might either be a group name or a group ID.
+
+=item $host = $class->get_fq_hostname()
+
+Returns the fully qualified host name of the machine.
 
 =cut
