fe2719d1b2f6623d9b41d7afb59bb2722930d362
Kjell-Magne Øierud <kjellm@redpill-linpro.com>
2009-03-25 09:17:25 +0000

    Forgot to include Munin::Node::Session in r1928

---
+++ node/lib/Munin/Node/Session.pm
@@ -0,0 +1,43 @@
+package Munin::Node::Session;
+
+use warnings;
+use strict;
+
+sub new {
+    my ($class) = @_;
+
+    my $self = {
+        tls_started  => 0,
+        peer_address => '',
+    };
+
+    return bless $self, $class;
+}
+
+
+1;
+
+__END__
+
+=head1 NAME
+
+Munin::Node::Session - FIX
+
+
+=head1 SYNOPSIS
+
+FIX
+
+
+=head1 METHODS
+
+=over
+
+=item B<new>
+
+ $class->new();
+
+FIX
+
+=back
+
