f9065a25c46f0d454784978875ee92b64c01805f
Kjell-Magne Øierud <kjellm@redpill-linpro.com>
2009-04-01 07:26:48 +0000

    Forgot to add this file in last commit

---
+++ node/lib/Munin/Node/Service.pm
@@ -0,0 +1,63 @@
+package Munin::Node::Service;
+
+use warnings;
+use strict;
+
+use Munin::Node::Config;
+
+
+my $config = Munin::Node::Config->instance();
+
+
+sub is_a_runnable_service {
+    my ($class, $file) = @_;
+    
+    my $path = "$config->{servicedir}/$file";
+
+    return unless -f $path && -x _;
+
+    # FIX isn't it enough to check that the file is executable and not
+    # in 'ignores'? Can hidden files and config files be
+    # unintantionally executable? What does config files do in the
+    # service directory? Shouldn't we complain if there is junk in the
+    # service directory?
+    return if $file =~ m/^\./;               # Hidden files
+    return if $file =~ m/.conf$/;            # Config files
+
+    return if $file !~ m/^([-\w.]+)$/;       # Skip if any weird chars
+    $file = $1;                              # Not tainted anymore.
+
+    foreach my $regex (@{$config->{ignores}}) {
+        return if $file =~ /$regex/;
+    }
+    
+    return 1;
+}
+
+
+1;
+
+__END__
+
+
+=head1 NAME
+
+Munin::Node::Service - FIX
+
+
+=head1 SYNOPSIS
+
+FIX
+
+
+=head1 METHODS
+
+=over
+
+=item B<is_a_runnable_service>
+
+ my $bool = Munin::Node::Service->is_a_runnable_service('foo');
+
+FIX
+
+=back
