e9f58c7c2589b794b7f2ae94868dddbf36be48a0
Kjell-Magne Øierud <kjellm@redpill-linpro.com>
2009-04-29 20:48:30 +0000

    Substitute @@FOO@@ in man pages

--- node/NodeBuilder.pm
+++ node/NodeBuilder.pm
@@ -6,22 +6,53 @@ use strict;
 
 use lib '../common/lib';
 
+use English qw(-no_match_vars);
 use File::Spec;
+use Munin::Common::Defaults;
 
 sub new {
     my ($class, %args) = @_;
 
     $args{sbin_files} = { map {$_ => $_} Module::Build::Base->_files_in('sbin') };
+
     my $self = $class->SUPER::new(%args);
-    push @{$self->{properties}{bindoc_dirs}}, 'blib/sbin';
-
-    $self->install_path('sbin' => $self->install_destination('bin')."/../sbin/");
-
     #use Data::Dumper; warn Dumper($self);
 
+    push @{$self->{properties}{bindoc_dirs}}, 'blib/sbin';
+    $self->install_path('sbin' => $self->install_destination('bin')."/../sbin/");
     $self->add_build_element('sbin');
 
     return $self;
 }
+
+
+sub ACTION_docs {
+    my ($self) = @_;
+
+    warn "HERE";
+
+    $self->SUPER::ACTION_docs;
+    my @files = (
+        Module::Build::Base->_files_in('blib/bindoc'),
+        Module::Build::Base->_files_in('blib/libdoc'),
+    );
+
+    use Data::Dumper; warn Dumper(\@files);
+
+    warn $Munin::Common::Defaults::MUNIN_CONFDIR;
+
+    for my $file (@files) {
+        open my $fh, '<', $file or die $OS_ERROR;
+        my $contents = do { local $INPUT_RECORD_SEPARATOR; <$fh> };
+        close $fh or die $OS_ERROR;
+
+        $contents =~ s/@@([^@]+)@@/eval "\$Munin::Common::Defaults::MUNIN_$1"/eg;
+
+        open $fh, '>', $file or die $OS_ERROR;
+        print $fh $contents;
+        close $fh or die $OS_ERROR;
+    }
+}
+
 
 1;
