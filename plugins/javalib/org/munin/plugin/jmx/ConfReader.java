7d9430d9db7075b39765ad6772a191e98e3f6afd
Tom Feiner <feiner.tom@gmail.com>
2009-10-22 15:19:57 +0000

    jmx_.in support for custom configuration using standard munin plugin-conf.d, no more need for /etc/munin/jmx.conf. A big thanks goes to Ilya Kikoin for his help on this plugin

--- plugins/javalib/org/munin/plugin/jmx/ConfReader.java
+++ plugins/javalib/org/munin/plugin/jmx/ConfReader.java
@@ -1,66 +1,41 @@
 package org.munin.plugin.jmx;
 
-import java.io.*;
-
 public class ConfReader {
-    
-    public void ConfReader(){}
 
-    public static String[] GetConnectionInfo(String servicename){
-
-        String[] connectionInfo = new String[2];
-        File f;
-        int i = 0;
-        try{
-       f = new File("/etc/munin/jmx.conf");
-        StringBuilder temp = new StringBuilder();
-
-        if (!f.exists() && f.length() < 0) {
-            System.out.println("The specified file does not exist");
-        } else {
-            FileInputStream finp = new FileInputStream(f);
-            byte b;
-            do {
-                b = (byte) finp.read();
-                if ((char) b != '\n') {
-                    temp.append((char) b);
-                } else {
-                    if (temp.toString().equals("SERVICE=" + servicename)) {
-                        temp = new StringBuilder();
-
-                        do {
-                            b = (byte) finp.read();
-                            if ((char) b != '\n') {
-                                temp.append((char) b);
-
-                            } else if (i == 2) {
-                                break;
-                            } else {
-                                connectionInfo[i++] = temp.toString();
-                                temp = new StringBuilder();
-
-                            }
-                        } while (b != -1);
-
-                        break;
-                    }
-                    temp = new StringBuilder();
-
-                }
-
-            } while (b != -1);
-
-            finp.close();
-        connectionInfo[0] = connectionInfo[0].substring(3);
-        connectionInfo[1] = connectionInfo[1].substring(5);
-        }
-        }catch(Exception e)
+    private static final String SERVICE_PARAM   = "SERVICE";
+    private static final String IP_PARAM        = "ip";
+    private static final String PORT_PARAM      = "port";
+                
+    public static String[] GetConnectionInfo(String servicename)
+    {
+        String service_value = System.getenv(SERVICE_PARAM);
+                                
+        if (service_value == null) 
         {
-            System.out.println(e);
+            System.out.println("SERVICE is not configured");
+            System.exit(1);
         }
-    
 
-        return connectionInfo;
+        if (!service_value.equals(servicename)) 
+        {
+            System.out.println("Service name does not match: " + servicename);
+            System.exit(1);
+        }
+
+        String ip   = System.getenv(IP_PARAM);
+        String port = System.getenv(PORT_PARAM);
+
+        if( ip == null || ip.equals("null") || port == null || port.equals("null") )
+        {
+            System.out.println(
+                                "The following parameters were not configured:" + 
+                                (ip == null ? " ip" : "") + 
+                                (port == null ? " port" : "") 
+                              );
+            System.exit(1);
+        }
+
+        return new String[]{ip , port};
     }
 }
 
