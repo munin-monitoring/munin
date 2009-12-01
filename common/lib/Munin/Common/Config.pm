15b93f84441bf52c7444112fa8197d71a58dd051
Nicolai Langfeldt <janl@redpill-linpro.com>
2009-12-01 13:16:06 +0000

    * Also add munin_cgi_graph_jobs to the legal keywords

--- common/lib/Munin/Common/Config.pm
+++ common/lib/Munin/Common/Config.pm
@@ -12,28 +12,32 @@ use English qw(-no_match_vars);
 # by the node or the master which use divergent logging facilities.
 
 my %legal = map { $_ => 1 } (
+
         "tmpldir", "ncsa", "ncsa_server", "ncsa_config", "rundir",
-	"dbdir", "logdir", "htmldir", "includedir", "domain_order", "node_order",
-	"graph_order", "graph_sources", "fork", "graph_title", "create_args",
-	"graph_args", "graph_vlabel", "graph_vtitle", "graph_total",
-	"graph_scale", "graph", "update", "host_name", "label", "cdef", "draw",
-	"graph", "max", "min", "negative", "skipdraw", "type", "warning",
-	"critical", "stack", "sum", "address",
-	"htaccess", "warn", "use_default_name", "use_node_name", "port",
-	"graph_noscale", "nsca", "nsca_server", "nsca_config", "extinfo",
-	"fetch_data", "filename", "max_processes", "nagios", "info",
-	"graph_info", "graph_category", "graph_strategy", "graph_width",
+	"dbdir", "logdir", "htmldir", "includedir", "domain_order",
+	"node_order", "graph_order", "graph_sources", "fork",
+	"graph_title", "create_args", "graph_args", "graph_vlabel",
+	"graph_vtitle", "graph_total", "graph_scale", "graph",
+	"update", "host_name", "label", "cdef", "draw", "graph",
+	"max", "min", "negative", "skipdraw", "type", "warning",
+	"critical", "stack", "sum", "address", "htaccess", "warn",
+	"use_default_name", "use_node_name", "port", "graph_noscale",
+	"nsca", "nsca_server", "nsca_config", "extinfo", "fetch_data",
+	"filename", "max_processes", "nagios", "info", "graph_info",
+	"graph_category", "graph_strategy", "graph_width",
 	"graph_height", "graph_sums", "local_address", "compare",
-	"text", "command", "contact", "contacts",  "max_messages",
-	"always_send", "notify_alias", "line", "state", "graph_period",
-	"cgiurl_graph", "cgiurl", "tls", "service_order", "category_order",
-	"version", "tls_certificate", "tls_private_key", "tls_pem",
-	"tls_verify_certificate", "tls_verify_depth", "tls_ca_certificate",
-	"graph_data_size", "colour", "graph_printf", "ok", "unknown",
-	"palette", "realservname", "cdef_name", "graphable", "process",
-	"realname", "onlynullcdef", "group_order", "pipe", "pipe_command",
-	"unknown_limit", "notify_countdown", "dropdownlimit", "max_graph_jobs",
-);
+	"text", "command", "contact", "contacts", "max_messages",
+	"always_send", "notify_alias", "line", "state",
+	"graph_period", "cgiurl_graph", "cgiurl", "tls",
+	"service_order", "category_order", "version",
+	"tls_certificate", "tls_private_key", "tls_pem",
+	"tls_verify_certificate", "tls_verify_depth",
+	"tls_ca_certificate", "graph_data_size", "colour",
+	"graph_printf", "ok", "unknown", "palette", "realservname",
+	"cdef_name", "graphable", "process", "realname",
+	"onlynullcdef", "group_order", "pipe", "pipe_command",
+	"unknown_limit", "notify_countdown", "dropdownlimit",
+	"max_graph_jobs", "munin_cgi_graph_jobs" );
 
 my %bools = map { $_ => 1} qw(yes no true false on off 1 0);
 
