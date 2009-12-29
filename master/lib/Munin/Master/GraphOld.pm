7de992ae5906d467447795f95f2f1a4644b172ff
Nicolai Langfeldt <janl@redpill-linpro.com>
2009-12-29 16:09:57 +0000

    * More specific test for watermark feature (1.2.13 required)

--- master/lib/Munin/Master/GraphOld.pm
+++ master/lib/Munin/Master/GraphOld.pm
@@ -1092,7 +1092,8 @@ sub process_service {
 
         my @complete = get_fonts();
 
-        push(@complete, '-W', $watermark) if $RRDs::VERSION >= 1.2;
+	# Watermarks introduced in RRD 1.2.13.
+        push(@complete, '-W', $watermark) if $RRDs::VERSION >= 1.2013;
 
         # Do the header (title, vtitle, size, etc...)
         push @complete, @{get_header($service, $time)};
