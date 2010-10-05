c1c2f2f967f708edec530142ed174ef016302156
Tom Feiner <feiner.tom@gmail.com>
2010-10-05 14:02:43 +0200

    Import Debian version 1.4.5-3

---
+++ debian/munin-node.cron.d
@@ -0,0 +1,12 @@
+#
+# cron-jobs for munin-node
+#
+
+MAILTO=root
+
+# If the APT plugin is enabled, update packages databases approx. once
+# an hour (12 invokations an hour, 1 in 12 chance that the update will
+# happen), but ensure that there will never be more than two hour (7200
+# seconds) interval between updates..
+*/5 * * * *	root if [ -x /etc/munin/plugins/apt_all ]; then /etc/munin/plugins/apt_all update 7200 12 >/dev/null; elif [ -x /etc/munin/plugins/apt ]; then /etc/munin/plugins/apt update 7200 12 >/dev/null; fi
+
