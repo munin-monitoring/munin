8e5615768c0f175e150ef36bf5a9750bb1443841
Jorne Kandziora <jorne@quarantainenet.nl>
2011-05-13 09:43:00 +0000

    Added defaults for update_rate and data_file_size to stop needlessly tuning of the data files

--- master/lib/Munin/Master/UpdateWorker.pm
+++ master/lib/Munin/Master/UpdateWorker.pm
@@ -624,6 +624,8 @@ sub _set_rrd_data_source_defaults {
     $data_source->{type} = 'GAUGE' unless defined($data_source->{type});
     $data_source->{min}  = 'U'     unless defined($data_source->{min});
     $data_source->{max}  = 'U'     unless defined($data_source->{max});
+    $data_source->{update_rate}  = '300'     unless defined($data_source->{update_rate});
+    $data_source->{graph_data_size}  = 'normal'     unless defined($data_source->{graph_data_size});
 }
 
 
