d7e3999b51044050eab409d493facbca6e0ba666
Steve Schnepp <steve.schnepp@pwkf.org>
2011-06-20 13:23:35 +0000

    - adding preliminary graphing abilities

--- contrib/rrd_perf.pl
+++ contrib/rrd_perf.pl
@@ -6,6 +6,19 @@ use strict;
 use warnings;
 
 use RRDs;
+use Time::HiRes;
+
+use Getopt::Long;
+
+my $graph_interval;
+my $graph_ratio = 100;
+my $verbose;
+
+GetOptions(
+	"g=i" => \$graph_interval,
+	"r=i" => \$graph_ratio,
+	"verbose" => \$verbose,
+) or die "invalid options";
 
 my $nb_rrd = (shift || 100);
 my $nb_rrd_per_dir = (shift || 100);
@@ -44,7 +57,27 @@ for (my $rrd_number=0; $rrd_number < $nb_rrd; $rrd_number ++) {
 	push @rrds, $rrdfilename;
 }
 
-# Simulate a munin's workload
+# Simulating munin's graphing in a sub process
+if ($graph_interval && ! fork()) {
+	while(1) {
+		my $offset = int rand($nb_rrd);
+
+		for (my $i = 0; $i < $nb_rrd * $graph_ratio / 100;  $i++) {
+			my $rrdfilename = $rrds[($offset + $i) % $nb_rrd];
+			print STDERR "graphing RRD $rrdfilename\n";
+			RRDs::graph(
+				'/dev/null',
+				"DEF:a=$rrdfilename:42:AVERAGE",
+				"LINE1:a",
+			);
+		}
+	} continue {
+		sleep 1 / $graph_interval;
+	}
+	exit;
+}
+
+# Simulate a munin's update workload
 while (1) {
 	my $epoch = time;
 	for my $rrdfilename (@rrds) {
