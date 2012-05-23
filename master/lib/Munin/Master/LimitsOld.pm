c57ae738df28fb654286aa96da6b4f5244c2f60b
Stig Sandbeck Mathisen <ssm@fnord.no>
2012-05-23 10:57:05 +0000

    Bugfix for munin-limits on hosts with multiple levels of domains, use "-" instead of " :: " in the file name)

--- master/lib/Munin/Master/LimitsOld.pm
+++ master/lib/Munin/Master/LimitsOld.pm
@@ -267,7 +267,7 @@ sub get_full_group_path {
     my $name       = get_notify_name($group);
 
     if (defined $parent and munin_get_node_name($parent) ne "root") {
-	return (get_full_group_path($parent) . " :: " . $name);
+	return (get_full_group_path($parent) . "-" . $name);
     } else {
 	return $name;
     }
