munin-monitoring/munin

commit 1aec747c10f0d5077842294ca351b918b77fb1e9

master: provide a default to graph_strategy.
This allows $config->{graph_strategy} to be usable instead of
requiring munin_get.

master/lib/Munin/Master/HTMLConfig.pm
@@ -482,7 +482,7 @@ sub generate_service_templates {
     }
 
     my $imgpath = $root_path;
-    if ( munin_get($config, "graph_strategy", "cron") eq "cgi" ) {
+    if ( $config->{graph_strategy} eq 'cgi' ) {
 	$imgpath = $config->{'cgiurl_graph'};
     }
 
@@ -508,7 +508,7 @@ sub generate_service_templates {
 	for my $scale (@times) {
         # Don't try to find the size if cgi is enabled, 
         # otherwise old data might pollute  
-        next if (munin_get($config, "graph_strategy", "cron") eq "cgi");
+        next if ($config->{graph_strategy} eq 'cgi');
         if (my ($w, $h)
             = get_png_size(munin_get_picture_filename($service, $scale))) {
             $srv{"img" . $scale . "width"}  = $w;
@@ -520,7 +520,7 @@ sub generate_service_templates {
         $srv{imgweeksum} = "$srv{node}-week-sum.png";
         $srv{imgyearsum} = "$srv{node}-year-sum.png";
         for my $scale (["week", "year"]) {
-            next if (munin_get($config, "graph_strategy", "cron") eq "cgi");
+            next if ($config->{graph_strategy} eq 'cgi');
             if (my ($w, $h)
                 = get_png_size(munin_get_picture_filename($service, $scale, 1)))
             {

master/lib/Munin/Master/Config.pm
@@ -145,6 +145,7 @@ my %booleans = map {$_ => 1} qw(
 		fork             => 1,
 		rrdcached_socket => "", # default is unused
 		graph_data_size  => 'normal',
+		graph_strategy   => 'cron',
 		groups           => {},
 		local_address    => 0,
 		logdir           => $Munin::Common::Defaults::MUNIN_LOGDIR,

commit d20222a415d8cf68a922a4d9d23f0643803488ee

html: fix bug introduced by 1aec747c10
Commit 1aec747c10f0d5077842294ca351b918b77fb1e9 failed to address the fix in
789c59e3a27f5d38cabd02c8f57fec605e6146a8 fully. So we somewhat revert
1aec747c10 in stable-2.0.

Yet, as 1aec747c10 is a big performance enhancer, it might be a good idea to
rework on it in devel. But for stable-2.0, the performance hit is acceptable.

Closes: D:689291

master/lib/Munin/Master/HTMLConfig.pm
@@ -481,7 +481,7 @@ sub generate_service_templates {
     }
 
     my $imgpath = $root_path;
-    if ( $config->{graph_strategy} eq 'cgi' ) {
+    if ( munin_get($config, "graph_strategy", "cron") eq "cgi" ) {
 	$imgpath = $config->{'cgiurl_graph'};
     }
 
@@ -505,7 +505,8 @@ sub generate_service_templates {
     }
 
 	for my $scale (@times) {
-		if (my ($w, $h) = get_png_size(munin_get_picture_filename($service, $scale))) {
+		my ($w, $h) = get_png_size(munin_get_picture_filename($service, $scale));
+		if ($w && $h) {
 			$srv{"img" . $scale . "width"}  = $w;
 			$srv{"img" . $scale . "height"} = $h;
 		}
@@ -516,10 +517,11 @@ sub generate_service_templates {
         $srv{imgyearsum} = "$srv{node}-year-sum.png";
 
         for my $scale (["week", "year"]) {
-			if (my ($w, $h) = get_png_size(munin_get_picture_filename($service, $scale, 1))) {
-				$srv{"img" . $scale . "sumwidth"}  = $w;
-				$srv{"img" . $scale . "sumheight"} = $h;
-			}
+		my ($w, $h) = get_png_size(munin_get_picture_filename($service, $scale, 1));
+		if ($w && $h) {
+			$srv{"img" . $scale . "sumwidth"}  = $w;
+			$srv{"img" . $scale . "sumheight"} = $h;
+		}
         }
     }
 
@@ -719,7 +721,7 @@ sub get_png_size {
     my $width    = undef;
     my $height   = undef;
 
-	return (undef, undef) if $config->{graph_strategy} eq "cgi";
+    return (undef, undef) if (munin_get($config, "graph_strategy", "cron") eq "cgi") ;
 
     if (open(my $PNG, '<', $filename)) {
         my $incoming;