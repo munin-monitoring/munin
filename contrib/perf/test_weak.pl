75c2d90119bd7bf3349f83cea8b27447f6c638dc
Steve Schnepp <steve.schnepp@pwkf.org>
2013-03-23 14:00:58 +0100

    contrib: complete test with Storable dump/load
    Test if Storable also saves the "weak" state.
    
    Hint: it does.

--- contrib/perf/test_weak.pl
+++ contrib/perf/test_weak.pl
@@ -3,23 +3,33 @@
 use warnings;
 use strict;
 
-use Storable;
+use File::Temp;
+use Data::Dumper;
+use Storable qw(nstore_fd fd_retrieve);
 use Scalar::Util qw(weaken isweak);
 
-my $number = 1000;
+my $number = shift || 10;
 my $a = {};
 
-for my $j (0 .. $number) {
-	$a = {};
-	for my $i (0 .. $number) {
-		$a->{$i} = {
-			"$i$i" => "$i$i",
-			"$i$i" => "$i$i",
-		};
-	}
-
-	for my $i (0 .. $number) {
-		$a->{$i}{next} = $a->{ ($i + 1) % $number };
-		weaken($a->{$i}{next}) if $ENV{WEAKEN};
-	}
+for my $i (0 .. $number) {
+	$a->{$i} = {
+		"$i$i" => "$i$i",
+		"$i$i" => "$i$i",
+	};
 }
+
+for my $i (0 .. $number) {
+	$a->{$i}{next} = $a->{ ($i + 1) % $number };
+	weaken($a->{$i}{next}) if $ENV{WEAKEN};
+}
+
+my $temp_storable = File::Temp->new();
+
+nstore_fd($a, $temp_storable);
+
+for my $j (0 .. ($number * $number)) {
+	$temp_storable->seek( 0, SEEK_SET );
+	$a = fd_retrieve($temp_storable);
+}
+
+print Dumper($a);
