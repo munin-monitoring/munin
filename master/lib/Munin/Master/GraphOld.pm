munin-monitoring/munin

commit 2fdef85b6e167e9b875d6f9bb2147974f599071e

master: fix field.trend field with long names
When configuring trends for a "df"-graph I noticed, that the name of the data
source was mangled and thus rrd complained about an "invalid rpn expression".

This commit uses the function "get_field_name" to mangle the name if necessary
(longer than 15 chars).

master/lib/Munin/Master/GraphOld.pm
@@ -1609,6 +1609,7 @@ sub handle_trends {
     foreach my $field (@{munin_find_field($service, "label")}) {
         my $fieldname = munin_get_node_name($field);
 	my $colour = $single_colour;
+        my $rrdname = get_field_name($fieldname)
 
 	# Skip virtual fieldnames, otherwise beware of $hash->{foo}{bar}. 
 	#
@@ -1628,8 +1629,8 @@ sub handle_trends {
 
         #trends
         if (defined $service->{$fieldname}{'trend'} and $service->{$fieldname}{'trend'} eq 'yes') {
-            push (@complete, "CDEF:t$fieldname=c$cdef$fieldname,$futuretime,TRENDNAN");
-            push (@complete, "LINE1:t$fieldname#$colour:$fieldname trend\\l");
+            push (@complete, "CDEF:t$rrdname=c$cdef$rrdname,$futuretime,TRENDNAN");
+            push (@complete, "LINE1:t$rrdname#$colour:$fieldname trend\\l");
             DEBUG "[DEBUG] set trend for $fieldname\n";
         }
 
@@ -1639,8 +1640,8 @@ sub handle_trends {
             my @predict = split(",", $service->{$fieldname}{'predict'});
             my $predictiontime = int ($futuretime / $predict[0]) + 2; #2 needed for 1 day
             my $smooth = $predict[1]*$resolutions{$time};
-            push (@complete, "CDEF:p$fieldname=$predict[0],-$predictiontime,$smooth,c$cdef$fieldname,PREDICT");
-            push (@complete, "LINE1:p$fieldname#$colour:$fieldname prediction\\l");
+            push (@complete, "CDEF:p$rrdname=$predict[0],-$predictiontime,$smooth,c$cdef$rrdname,PREDICT");
+            push (@complete, "LINE1:p$rrdname#$colour:$fieldname prediction\\l");
             DEBUG "[DEBUG] set prediction for $fieldname\n";
         }
     }

commit f2f06a444c9cf533e31ebb4b1684e7f35404728a

master: fix typo from 2fdef85
2fdef85b6e167e9b875d6f9bb2147974f599071e introduced a typo. Fixing it.

master/lib/Munin/Master/GraphOld.pm
@@ -1609,7 +1609,7 @@ sub handle_trends {
     foreach my $field (@{munin_find_field($service, "label")}) {
         my $fieldname = munin_get_node_name($field);
 	my $colour = $single_colour;
-        my $rrdname = get_field_name($fieldname)
+        my $rrdname = get_field_name($fieldname);
 
 	# Skip virtual fieldnames, otherwise beware of $hash->{foo}{bar}. 
 	#