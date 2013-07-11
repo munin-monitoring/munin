5ca4a7ca28bf9a04c8b26c162085022957fd6a73
Steve Schnepp <steve.schnepp@pwkf.org>
2013-07-11 22:04:39 +0200

    master: adding translucent AREA+STACK

--- master/lib/Munin/Master/GraphOld.pm
+++ master/lib/Munin/Master/GraphOld.pm
@@ -882,6 +882,7 @@ sub process_service {
     my $now          = time;
     my $fnum         = 0;
     my @rrd;
+    my @rrd_overlay;
 
     DEBUG "[DEBUG] Node name: $sname\n";
 
@@ -1111,12 +1112,24 @@ sub process_service {
 	my $period  = munin_get($service, "graph_period", "second");
 	$tmplabel =~ s/\$\{graph_period\}/$period/g;
 
+       if ($fielddraw eq "AREA" || $fielddraw eq "STACK") {
+		my $fielddraw_overlay = ($fielddraw eq "STACK" ? "STACK" : "LINE.5");
+                push(@rrd, $fielddraw . ":g$rrdname" . "#$colour" ."6F"
+			. ":". escape($tmplabel)
+			. (" " x ($max_field_len + 1 - length $tmplabel))
+);
+		push(@rrd_overlay, $fielddraw_overlay
+			. ":g$rrdname"
+			. "#$colour"
+);
+	} else {
         push(@rrd,
                   $fielddraw
                 . ":g$rrdname"
                 . "#$colour" . ":"
                 . escape($tmplabel)
                 . (" " x ($max_field_len + 1 - length $tmplabel)));
+	}
 
         # Check for negative fields (typically network (or disk) traffic)
         if ($has_negative) {
@@ -1371,7 +1384,7 @@ sub process_service {
                 $line;
             } @rrd;
         }
-        push @complete, @rrd;
+        push @complete, @rrd, @rrd_overlay;
 
         # graph end in future
         push (@complete, handle_trends($time, $lastupdate, $pinpoint, $service, $RRDkludge, @added));
