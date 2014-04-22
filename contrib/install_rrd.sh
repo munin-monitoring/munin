d478738011e79a7cfb04971ad18cd63d4341b5c7
Steve Schnepp <steve.schnepp@pwkf.org>
2014-04-22 14:35:47 +0200

    travis: fix rrd tar extract

--- contrib/install_rrd.sh
+++ contrib/install_rrd.sh
@@ -9,5 +9,5 @@ sudo apt-get install libpango1.0-dev libxml2-dev
 
 # Download a fixed version
 wget http://oss.oetiker.ch/rrdtool/pub/rrdtool-1.4.8.tar.gz
-tar -xzvf rrdtool-1.4.8
+tar -xzvf rrdtool-1.4.8.tar.gz
 cd rrdtool-1.4.8 && ./configure --prefix=/usr && make && make install
