3d130c0751dfb1cac3cf87ca0f1b79779ff136c7
Steve Schnepp <steve.schnepp@pwkf.org>
2014-04-22 14:48:29 +0200

    travis: customized rrd build

--- contrib/install_rrd.sh
+++ contrib/install_rrd.sh
@@ -10,4 +10,19 @@ sudo apt-get install libpango1.0-dev libxml2-dev
 # Download a fixed version
 wget http://oss.oetiker.ch/rrdtool/pub/rrdtool-1.4.8.tar.gz
 tar -xzvf rrdtool-1.4.8.tar.gz
-cd rrdtool-1.4.8 && ./configure --prefix=/usr && make && make install
+cd rrdtool-1.4.8
+./configure \
+	--disable-dependency-tracking \
+	--disable-rrdcgi \
+	--disable-mmap \
+	--disable-pthread \
+	--disable-ruby \
+	--disable-lua \
+	--disable-tcl \
+	--disable-python \
+	--disable-libdbi \
+	--disable-libwrap \
+	# Leave at the end
+
+make
+make install
