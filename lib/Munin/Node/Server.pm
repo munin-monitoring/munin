a668f4742a842213391a8fd5784466599a8c99f8
Stig Sandbeck Mathisen <ssm@fnord.no>
2015-03-19 22:24:13 +0100

    bugfix: "scalar %hash" does not return number of keys
    - "keys %hash" will, in scalar context
    
    - otherwise "scalar keys %hash"

--- lib/Munin/Node/Server.pm
+++ lib/Munin/Node/Server.pm
@@ -329,7 +329,7 @@ sub _print_service {
 sub _list_services {
     my ($session, $node) = @_;
 
-    if (scalar %nodes == 1 && ! exists $nodes{$node}) {
+    if (keys %nodes == 1 && ! exists $nodes{$node}) {
 	    # Only one node. Naming mismatch. Just give the use what he wants.
 	    ($node) = keys %nodes;
     }
