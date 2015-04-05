759f3246df068b0605ec2ac7ff64debd2c983d66
Quentin S <chteuchteu@gmail.com>
2015-04-05 23:01:39 +0200

    Time range switch: avoid conflicts between both

--- web/static/nodeview-timerangeswitch.js
+++ web/static/nodeview-timerangeswitch.js
@@ -11,11 +11,27 @@ $(document).ready(function() {
         var currentRange = $(this).parent().find('.selected').first().text();
         var newRange = $(this).text();
 
+        // Image may still be loading (if the user is really quick on switching time range)
+        // Let's remove any loading image if necessary
+        $('.graph_loading[data-timerange="' + currentRange + '"]').remove();
+
         // Remove "selected" attribute
         $(this).parent().find('li').removeClass('selected');
 
         // Add "selected" class to this
         $(this).addClass('selected');
+
+        // Add "disabled" class to the other time range switch
+        var thisRSIndex = $(this).parent().parent().index();
+        var otherRS = $($('.timeRangeSwitch')[thisRSIndex == 1 ? 0 : 1]);
+        var otherLi = otherRS.find('li');
+        otherLi.removeClass('disabled');
+        otherLi.each(function() {
+            if ($(this).text() == newRange)
+                $(this).addClass('disabled');
+        });
+
+
 
         // Replace src attribute of current column (all images that matches current range)
         // => contains "-day." for day (don't force extension since there can be pngs/svgs)
@@ -31,7 +47,7 @@ $(document).ready(function() {
         // Show a loading spinner on each changed image
         images.css('opacity', '0.7');
         var r_path = $('#r_path').val();
-        images.after('<img src="' + r_path + '/static/loading.gif" class="graph_loading" />');
+        images.after('<img src="' + r_path + '/static/loading.gif" class="graph_loading" data-timerange="' + newRange + '" />');
 
         // Detect image load
         images.on('load', function() {
