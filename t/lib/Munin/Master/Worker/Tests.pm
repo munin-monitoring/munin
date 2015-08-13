52d628e639624f167195c473eee85dbca30dffca
Stig Sandbeck Mathisen <ssm@fnord.no>
2015-08-13 21:05:24 +0200

    Add tests for Munin::Master::Worker

---
+++ t/lib/Munin/Master/Worker/Tests.pm
@@ -0,0 +1,31 @@
+package Munin::Master::Worker::Tests;
+use base qw(Test::Class);
+use Test::More;
+# use Test::Deep;
+
+use Munin::Master::Worker;
+
+sub setup : Test(setup) {
+    my $self = shift;
+    $self->{worker} = Munin::Master::Worker->new('worker with id');
+    $self->{worker_noid} = Munin::Master::Worker->new();
+}
+
+sub class : Test {
+    my $self = shift;
+    my $worker = $self->{worker};
+    isa_ok($worker, 'Munin::Master::Worker');
+}
+
+sub class_noparameter : Test {
+    my $worker = shift->{worker_noid};
+    isa_ok($worker, 'Munin::Master::Worker');
+}
+
+sub method__to_string : Test(2) {
+    my $worker = shift->{worker};
+    can_ok($worker, 'to_string');
+    ok($worker->to_string, 'to_string returns a true value');
+}
+
+1;
