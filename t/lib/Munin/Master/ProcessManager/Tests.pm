196351fbb6883f20614cd76f8d827434d6226f37
Stig Sandbeck Mathisen <ssm@fnord.no>
2015-08-13 21:46:43 +0200

    Improve test coverage of Munin::Master::ProcessManager

--- t/lib/Munin/Master/ProcessManager/Tests.pm
+++ t/lib/Munin/Master/ProcessManager/Tests.pm
@@ -4,14 +4,31 @@ use Test::More;
 # use Test::Deep;
 
 use Munin::Master::ProcessManager;
+use Munin::Master::Worker;
 
 sub setup : Test(setup) {
     my $self = shift;
 
     $self->{manager} = Munin::Master::ProcessManager->new(
-        sub{ ok(1, 'async complete')}
-    );
+        sub{ ok(1, 'result_callback reached') },
+        sub{ ok(0, 'error_callback reached') },
+        );
 
+    $self->{worker} = Munin::Master::Worker->new();
+}
+
+sub class_without_result_callback : Test {
+    my $self = shift;
+
+    ok(!eval{Munin::Master::ProcessManager->new},
+       'should fail without mandatory result_callback');
+}
+
+sub class_without_error_callback : Test {
+    my $self = shift;
+
+    ok(Munin::Master::ProcessManager->new(sub{1}),
+       'should not fail without optional error_callback');
 }
 
 sub class : Test(1) {
@@ -19,14 +36,33 @@ sub class : Test(1) {
     isa_ok($manager, 'Munin::Master::ProcessManager');
 }
 
-sub method__add_workers : Test {
-    my $manager = shift->{manager};
+sub method__add_workers : Test(3) {
+    my $self    = shift;
+    my $manager = $self->{manager};
+    my $worker  = $self->{worker};
+
     can_ok($manager, 'add_workers');
+    ok($manager->add_workers($worker));
+
+    ok(!eval{$manager->add_workers('test imposter')},
+       'Adding non-worker objects should fail');
 }
 
-sub method__start_work : Test {
-    my $manager = shift->{manager};
+sub method__start_work : Test(3) {
+    my $self = shift;
+    my $manager = $self->{manager};
+    my $worker = $self->{worker};
+
     can_ok($manager, 'start_work');
+
+    # After this step, tests requires sockets and processes.
+    return('Set TEST_MEDIUM to run these tests')
+        unless $ENV{TEST_MEDIUM};
+
+    ok($manager->add_workers($worker), 'setup, add worker');
+
+    local $TODO = 'testing of start_work unimplemented';
+    ok(eval{$manager->start_work});
 }
 
 1;
