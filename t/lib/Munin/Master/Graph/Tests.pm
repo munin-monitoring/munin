0a0999e3e05d2f3385860a489391ce8ddf22a987
Stig Sandbeck Mathisen <ssm@fnord.no>
2015-08-17 14:42:11 +0200

    Add minimal testing for Munin::Master::Graph

---
+++ t/lib/Munin/Master/Graph/Tests.pm
@@ -0,0 +1,20 @@
+package Munin::Master::Graph::Tests;
+use base qw(Test::Class);
+use Test::More;
+
+sub class : Test(1) {
+    use_ok(Munin::Master::Graph);
+}
+
+sub function__is_ext_handled : Test(3) {
+    ok( !Munin::Master::Graph::is_ext_handled(),
+        'Should return a false value when run without params' );
+
+    ok( Munin::Master::Graph::is_ext_handled('PNG'),
+        'Should return a true value when argument is a supported extension' );
+
+    ok( !Munin::Master::Graph::is_ext_handled('SOMETHING'),
+        'Should return a false value when argument is an unsupported extension'
+    );
+}
+1;
