bca914dcdf5235471790d29b5ad2bc52f65af803
Lars Kruse <devel@sumpfralle.de>
2018-06-22 00:23:26 +0200

    remove outdated cached files (Closes: #700298)

--- debian/munin.cron.d
+++ debian/munin.cron.d
@@ -6,3 +6,7 @@ MAILTO=root
 
 */5 * * * *     munin if [ -x /usr/bin/munin-cron ]; then /usr/bin/munin-cron; fi
 14 10 * * *     munin if [ -x /usr/share/munin/munin-limits ]; then /usr/share/munin/munin-limits --force --contact nagios --contact old-nagios; fi
+
+# remove stale generated html and graph files (e.g. disabled plugins or fields with volatile names)
+27 03 * * *     munin if [ -d /var/cache/munin/www ]; then find /var/cache/munin/www/ -type f -name "*.html" -mtime +30 -delete; find /var/cache/munin/www/ -mindepth 1 -type d -empty -delete; fi
+32 03 * * *     www-data if [ -d /var/lib/munin/cgi-tmp ]; then find /var/lib/munin/cgi-tmp/ -type f -mtime +1 -delete; find /var/lib/munin/cgi-tmp/ -mindepth 1 -type d -empty -delete; fi
