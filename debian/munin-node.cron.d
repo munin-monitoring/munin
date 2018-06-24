1aa61456b4c3e089b2502bd29d31ee968971498e
Lars Kruse <devel@sumpfralle.de>
2018-06-24 02:22:23 +0200

    fix trivial spelling mistakes below debian/ (thanks "codespell")

--- debian/munin-node.cron.d
+++ debian/munin-node.cron.d
@@ -5,7 +5,7 @@
 MAILTO=root
 
 # If the APT plugin is enabled, update packages databases approx. once
-# an hour (12 invokations an hour, 1 in 12 chance that the update will
+# an hour (12 invocations an hour, 1 in 12 chance that the update will
 # happen), but ensure that there will never be more than two hour (7200
 # seconds) interval between updates..
 */5 * * * *	root if [ -x /etc/munin/plugins/apt_all ]; then /etc/munin/plugins/apt_all update 7200 12 >/dev/null; elif [ -x /etc/munin/plugins/apt ]; then /etc/munin/plugins/apt update 7200 12 >/dev/null; fi
