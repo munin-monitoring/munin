munin-monitoring/munin

commit 82c1707c4994f62d132b5d09d9ff2cfaac8aceec

update: use a persistent sqlite for state

lib/Munin/Master/LimitsOld.pm
@@ -316,17 +316,18 @@ sub process_service {
 
     use DBI;
     my $datafilename = $ENV{MUNIN_DBURL} || $config->{dbdir}."/datafile.sqlite";
+    my $datafilename_state = $ENV{MUNIN_DBURL} || $config->{dbdir}."/datafile-state.sqlite";
     DEBUG "[DEBUG] opening sql $datafilename";
     my $dbh = DBI->connect("dbi:SQLite:dbname=$datafilename","","") or die $DBI::errstr;
-    my $sth_state = $dbh->prepare('SELECT last_epoch, last_value, prev_epoch, prev_value, alarm FROM state WHERE id = ? and type = ?');
-    my $sth_state_upt = $dbh->prepare('UPDATE state SET alarm = ? WHERE id = ? and type = ?');
+    my $dbh_state = DBI->connect("dbi:SQLite:dbname=$datafilename","","") or die $DBI::errstr;
+    my $sth_state = $dbh_state->prepare('SELECT last_epoch, last_value, prev_epoch, prev_value, alarm FROM state WHERE id = ? and type = ?');
+    my $sth_state_upt = $dbh_state->prepare('UPDATE state SET alarm = ? WHERE id = ? and type = ?');
     my $sth_ds = $dbh->prepare('SELECT id FROM url WHERE path = ? and type = ?');
 
     foreach my $field (@$children) {
         next if (!defined $field or ref($field) ne "HASH");
         my $fname   = munin_get_node_name($field);
         my $fpath   = munin_get_node_loc($field);
-        my $onfield = munin_get_node($oldnotes, $fpath);
         my $oldstate = 'ok';
 
 
@@ -350,7 +351,7 @@ sub process_service {
 		$sth_state->execute($ds_id, "ds");
 		my ($current_updated_timestamp, $current_updated_value,
 			$previous_updated_timestamp, $previous_updated_value,
-			$old_alarm) = $sth_state->fetchrow_array;
+			$old_alarm, $num_unknowns) = $sth_state->fetchrow_array;
 
 		my $oldstate = $old_alarm || 'ok';
 

lib/Munin/Master/Update.pm
@@ -426,16 +426,29 @@ sub _dump_into_sql {
 	# values, along with a precomputed alarm state (UNKNOWN, NORMAL,
 	# WARNING, CRITICAL)
 	#
+	# But this is in a persistent DB. As it is stateful.
+	# Note that the whole DB will be stateful in the future, but this table
+	# is highly volatile anyway
+	my $datafilename_state = $datafilename;
+	$datafilename_state =~ s/\.sqlite$/-state.sqlite/;
+	my $dbh_state = DBI->connect("dbi:SQLite:dbname=$datafilename_state","","") or die $DBI::errstr;
+	#
 	# Note, this table is referenced by composite key (type,id) in order to be
 	# able to have any kind of states. Such as whole node states for example.
-	$dbh->do("CREATE TABLE IF NOT EXISTS state (id INTEGER, type VARCHAR,
+	$dbh_state->do("CREATE TABLE IF NOT EXISTS state (id INTEGER, type VARCHAR,
 		last_epoch INTEGER, last_value VARCHAR,
 		prev_epoch INTEGER, prev_value VARCHAR,
 		alarm VARCHAR, num_unknowns INTEGER
 		)");
-	$dbh->do("CREATE UNIQUE INDEX IF NOT EXISTS pk_state ON state (type, id)");
-	my $sth_state = $dbh->prepare('INSERT INTO state (id, type, last_epoch, last_value, prev_epoch, prev_value) VALUES (?, ?, ?, ?, ?, ?)');
-	$sth_state->{RaiseError} = 1;
+	$dbh_state->do("CREATE UNIQUE INDEX IF NOT EXISTS pk_state ON state (type, id)");
+	my $sth_state = $dbh_state->prepare('SELECT last_epoch, last_value, prev_epoch, prev_value, alarm, num_unknowns
+		FROM state WHERE id = ? AND type = ?');
+	my $sth_state_i = $dbh_state->prepare(
+		'INSERT INTO state (last_epoch, last_value, prev_epoch, prev_value, id, type) VALUES (?, ?, ?, ?, ?, ?)');
+	my $sth_state_u = $dbh_state->prepare(
+		'UPDATE state SET last_epoch = ?, last_value = ?, prev_epoch = ?, prev_value = ? WHERE id = ? AND type = ?');
+	$sth_state_i->{RaiseError} = 0;
+	$sth_state_u->{RaiseError} = 0;
 
 
 	# Configuration
@@ -574,7 +587,19 @@ sub _dump_into_sql {
 				INFO "No state found for ds $ds_id ($rrdfile_prefix)" unless $state_ds;
 				next unless $state_ds;
 
-				$sth_state->execute($ds_id, "ds", @{ $state_ds->{current} }, @{ $state_ds->{previous} }, );
+				$sth_state_u->execute(
+						@{ $state_ds->{current} },
+						@{ $state_ds->{previous} },
+						$ds_id, "ds",
+					);
+				if ($sth_state_u->rows == 0) {
+					# No row updated, go insert !
+					$sth_state_i->execute(
+						@{ $state_ds->{current} },
+						@{ $state_ds->{previous} },
+						$ds_id, "ds",
+					);
+				}
 
 				# Insert the rrd:last in the main DB
 				$sth_ds_attr->execute($ds_id, "rrd:last", $state_ds->{current}[0] );

commit 73443a447092bf74aad04c9f6121765e3580ecff

limits: replace remaining '$onfield' references
These lingering data field accesses were broken since 82c1707c4.

lib/Munin/Master/LimitsOld.pm
@@ -345,7 +345,6 @@ sub process_service {
         my $field   = munin_get_node($hash, [$fname]);
         next if (!defined $field or ref($field) ne "HASH");
         my $fpath   = munin_get_node_loc($field);
-        my $oldstate = 'ok';
 
         my ($warn, $crit, $unknown_limit) = get_limits($field);
 
@@ -362,9 +361,9 @@ sub process_service {
         $sth_state->execute($ds_id, "ds");
         my ($current_updated_timestamp, $current_updated_value,
             $previous_updated_timestamp, $previous_updated_value,
-            $old_alarm, $old_num_unknowns) = $sth_state->fetchrow_array;
+            $old_state, $old_num_unknowns) = $sth_state->fetchrow_array;
 
-        $oldstate = $old_alarm || 'ok';
+        $old_state ||= 'ok';
 
 		my $heartbeat = 600; # XXX - $heartbeat is a fixed 10 min (2 runs of 5 min).
 		if (! defined $current_updated_value || $current_updated_value eq "U") {
@@ -439,40 +438,41 @@ sub process_service {
                     : "Value is unknown.";
             my $num_unknowns;
 
-            if ( $oldstate ne "unknown") {
+            if ($old_state ne "unknown") {
+                # Assume that the state changed - see some exceptions below.
                 $hash->{'state_changed'} = 1;
             }
             else {
                 $hash->{'state_changed'} = 0;
             }
 
-            # First we'll need to check whether the user wants to ignore
-            # a few UNKNOWN values before actually changing the state to
-            # UNKNOWN.
+            # The user may want to ignore a few unknown values before
+            # they should be reported. Thus we need to track the number
+            # of recently received unknown values. And we may want to
+            # postpone a "change" notification until we reach the limit.
             if ($unknown_limit > 1) {
-                if (defined $onfield and defined $onfield->{"state"}) {
-                    if ($onfield->{"state"} ne "unknown") {
-                        if (defined $onfield->{"num_unknowns"}) {
-                            if ($onfield->{"num_unknowns"} < $unknown_limit) {
-                                # Don't change the state to UNKNOWN yet.
-                                $hash->{'state_changed'} = 0;
-                                $state = $onfield->{"state"};
-                                $extinfo = $onfield->{$state};
-
-                                # Increment the number of UNKNOWN values seen.
-                                $num_unknowns = $onfield->{"num_unknowns"} + 1;
-                            }
-                        }
-                        else {
+                if ($old_state ne "unknown") {
+                    # The last sample reported a different state.
+                    if ($old_num_unknowns) {
+                        # One or more previous values were also "unknown".
+                        if ($old_num_unknowns < $unknown_limit) {
                             # Don't change the state to UNKNOWN yet.
                             $hash->{'state_changed'} = 0;
-                            $state = $onfield->{"state"};
-                            $extinfo = $onfield->{$state};
-                            
-                            # Start counting the number of consecutive UNKNOWN
-                            # values seen.
-                            $num_unknowns = 1;
+                            $state = $old_state;
+                            $extinfo = $field->{"extinfo"};
+
+                            # Increment the number of UNKNOWN values seen.
+                            $num_unknowns = $old_num_unknowns + 1;
                         }
+                    } else {
+                        # The previous values were not reported as unknown.
+                        $hash->{'state_changed'} = 0;
+                        $state = $old_state;
+                        $extinfo = $field->{"extinfo"};
+
+                        # Start counting the number of consecutive UNKNOWN
+                        # values seen.
+                        $num_unknowns = 1;
                     }
                 }
             }
@@ -522,7 +522,7 @@ sub process_service {
                         . ") exceeded"
                 ));
 
-            if ( $oldstate ne "critical") {
+            if ($old_state ne "critical") {
                 $hash->{'state_changed'} = 1;
             }
         }
@@ -545,7 +545,7 @@ sub process_service {
                         . ") exceeded"
                 ));
 
-            if ( $oldstate ne "warning") {
+            if ($old_state ne "warning") {
                 $hash->{'state_changed'} = 1;
             }
         }
@@ -553,8 +553,8 @@ sub process_service {
             munin_set_var_loc(\%notes, [@$fpath, "state"], "ok");
             munin_set_var_loc(\%notes, [@$fpath, "ok"],    "OK");
 
-	    if ($oldstate ne 'ok') {
-                if ($oldstate eq 'unknown' && munin_get_bool($hobj, 'ignore_unknown', 'false')) {
+	    if ($old_state ne 'ok') {
+                if ($old_state eq 'unknown' && munin_get_bool($hobj, 'ignore_unknown', 'false')) {
                     DEBUG("[DEBUG] ignoring transition from UNKNOWN to OK");
                 } else {
 		    $hash->{'state_changed'} = 1;
@@ -564,7 +564,7 @@ sub process_service {
         }
 
 	# Replicate the state into the SQL DB
-	my $new_state = $onfield->{"state"};
+	my $new_state = $old_state;
 	$sth_state_upt->execute($new_state, $ds_id, "ds");
     }
     generate_service_message($hash);