8ff5d5a9ec8b7ffd99dce61fee42defe7bda3b29
Lars Kruse <devel@sumpfralle.de>
2018-12-15 05:23:07 +0100

    Node: "list" handles empty node name even with mixed-case own node_name
    Previously the "host_name" of a munin-node instance with mixed-case
    letters would not match an empty "list" request, if more than one local
    node was available (i.e. via virtual nodes provided by SNMP plugins).
    
    Now the empty list always works even for a mixed-case "host_name"
    configuration setting.

--- lib/Munin/Node/Server.pm
+++ lib/Munin/Node/Server.pm
@@ -337,7 +337,7 @@ sub _print_service {
 
 sub _list_services {
     my ($session, $node) = @_;
-    $node ||= $config->{fqdn};
+    $node ||= lc($config->{fqdn});
     if (keys %nodes == 1 && ! exists $nodes{$node}) {
 	    # Only one node. Naming mismatch. Just give the use what he wants.
 	    ($node) = keys %nodes;
