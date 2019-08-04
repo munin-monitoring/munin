0c6be797cbd4391898908af08061bcb407480382
Lars Kruse <devel@sumpfralle.de>
2019-08-04 15:18:44 +0200

    Cache cleanup: use configured settings for "htmldir" and "cgitmpdir"
    Previously the default values where used when cleaning up old cache
    files in a cron job.
    
    Closes: #933849

--- debian/munin.cron.d
+++ debian/munin.cron.d
@@ -8,5 +8,5 @@ MAILTO=root
 14 10 * * *     munin if [ -x /usr/share/munin/munin-limits ]; then /usr/share/munin/munin-limits --force --contact nagios --contact old-nagios; fi
 
 # remove stale generated html and graph files (e.g. disabled plugins or fields with volatile names)
-27 03 * * *     munin if [ -d /var/cache/munin/www ]; then find /var/cache/munin/www/ -type f -name "*.html" -mtime +30 -delete; find /var/cache/munin/www/ -mindepth 1 -type d -empty -delete; fi
-32 03 * * *     www-data if [ -d /var/lib/munin/cgi-tmp ]; then find /var/lib/munin/cgi-tmp/ -type f -mtime +1 -delete; find /var/lib/munin/cgi-tmp/ -mindepth 1 -type d -empty -delete; fi
+27 03 * * *     munin htmldir=$({ cat /etc/munin/munin.conf /etc/munin/munin-conf.d/* 2>/dev/null || true; } | sed -nE 's/^\s*htmldir\s+(\S.*)$/\1/p' | tail -1); htmldir=${htmldir:-/var/cache/munin/www}; if [ -d "$htmldir" ]; then find "$htmldir/" -type f -name "*.html" -mtime +30 -delete; find "$htmldir/" -mindepth 1 -type d -empty -delete; fi
+32 03 * * *     www-data cgitmpdir=$({ cat /etc/munin/munin.conf /etc/munin/munin-conf.d/* 2>/dev/null || true; } | sed -nE 's/^\s*cgitmpdir\s+(\S.*)$/\1/p' | tail -1); cgitmpdir=${cgitmpdir:-/var/lib/munin/cgi-tmp}; if [ -d "$cgitmpdir" ]; then find "$cgitmpdir/" -type f -mtime +1 -delete; find "$cgitmpdir/" -mindepth 1 -type d -empty -delete; fi
