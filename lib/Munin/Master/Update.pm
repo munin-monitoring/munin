70a2b14688a729bde6fdddbe6ef875b93e4fa9c6
Steve Schnepp <steve.schnepp@pwkf.org>
2023-03-08 21:55:34 +0100

    m/update: reuse the db connection within a process

--- lib/Munin/Master/Update.pm
+++ lib/Munin/Master/Update.pm
@@ -84,7 +84,7 @@ sub get_dbh {
 	use Carp;
 	$db_args{HandleError} = sub { confess(shift) };
 
-	my $dbh = DBI->connect("dbi:$db_driver:dbname=$datafilename", $db_user, $db_passwd, \%db_args) or die $DBI::errstr;
+	my $dbh = DBI->connect_cached("dbi:$db_driver:dbname=$datafilename", $db_user, $db_passwd, \%db_args) or die $DBI::errstr;
 
 	DEBUG 'get_dbh: $dbh->{Driver}->{Name} = ' . $dbh->{Driver}->{Name};
 
@@ -200,7 +200,12 @@ sub _run_workers {
 			$res = $worker->do_work();
 		};
 
-		$worker->{dbh}->disconnect();
+		# Disconnect via removing it from scope
+		#
+		# We don't want to call disconnect forcefully, in order to
+		# fully leverage the reference counting and the automatic call
+		# to ->DESTROY
+		delete($worker->{dbh});
 
 		my $worker_id = $worker->{ID};
 		if (! $res || $@) {
