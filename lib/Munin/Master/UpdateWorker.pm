ab9874616dd3903d56498d36f6276de85b741387
Mike Tillberg <mt@kyossystems.com>
2023-07-02 14:04:04 -0400

    prevent malicious config lines

--- lib/Munin/Master/UpdateWorker.pm
+++ lib/Munin/Master/UpdateWorker.pm
@@ -647,6 +647,12 @@ sub uw_handle_config {
 			next; # Handled
 		}
 
+		# Prevent plugins from trying to set rrd:* attrs
+		if ($arg2 =~ /^rrd:/) {
+			WARN "Invalid line: $line";
+			next;
+		}
+
 		$fields{$arg1}{$arg2} = $value;
 
 		# Adding the $field if not present.
