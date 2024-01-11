3e2c0982fec55092e2b8f6ea7ba81c830f017d20
Kim B. Heino <b@bbbs.net>
2024-01-11 17:49:52 +0200

    plugins/postgres: use standard category "db" instead of "PostgreSQL"

--- lib/Munin/Plugin/Pgsql.pm
+++ lib/Munin/Plugin/Pgsql.pm
@@ -123,7 +123,7 @@ use Munin::Plugin;
                 database is an older version than this, the plugin will exit
                 with an error.
  category       The category for this plugin. Copied directly to the config
-                output. Default 'PostgreSQL'.
+                output. Default 'db'.
  title          The title for this plugin. Copied directly to the config output.
  info           The info for this plugin. Copied directly to the config output.
  vlabel         The vertical label for the graph. Copied directly to the config
@@ -223,7 +223,7 @@ sub new {
 
     my %defaults = (
         base      => 1000,
-        category  => 'PostgreSQL',
+        category  => 'db',
         graphdraw => 'LINE1',
         graphtype => 'GAUGE',
     );
